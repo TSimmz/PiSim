3a24dcbd20b15cee30d49ac5e02f9404431276ff
tsimoni <tyler.simoni@caemilusa.com>
2018-04-03 10:16:32 -0400

    added globals and functions

--- simstart.py
+++ simstart.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from threading import Thread
 
 ###########################################
 # define argument variables
@@ -7,19 +8,41 @@
 ###########################################
 # initialize global variables
 ###########################################
+p = 0
+r = 0
+y = 0
 
 ###########################################
 # input 
 #   calculate pitch, roll, yaw from 
 #   controller input
 ###########################################
+def input(threadname):
+    global p
+    global r
+    global y
+
+    pass
 
 ###########################################
 # output
 #   pitch, roll, yaw to arduino
 ###########################################
+def output(threadname):
+    pass
 
 ###########################################
 # main 
 #   initialize child threads
 ###########################################
+def main():
+    input_thread = Thread(target=input, args=("input_thread",))
+    output_thread = Thread(target=output, args=("output_thread"))
+
+
+
+###########################################
+# execute main 
+###########################################
+if __name__ == "__main__":
+    main():
